f6228046dea3b77b78389a272e3d8ce1b1fd487f
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-19 23:29:24 -0700

    update srcswjs w asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,29 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+registerRoute = (({ request }) => ['worker', 'style', 'script'].includes(request.destination), new CacheFirst({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+}));
+
+registerRoute = (({ request }) => request.destination === 'image', new CacheFirst({
+  cacheName: 'image-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+}));
+  
+
 registerRoute();
